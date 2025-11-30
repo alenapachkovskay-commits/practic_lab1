6b5f18387946d9b54e973cb1b96c593e71fcc2a3
Admin <alenapachkovskay@gmail.com>
2025-12-01 01:53:00 +0700

    Initial commit

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Book, Author, BookInstance, Genre
 from django.views import generic
+from django.contrib.auth.mixins import LoginRequiredMixin
 def index(request):
     """
         Функция отображения для домашней страницы сайта.
     """
+    num_authors = Author.objects.count()
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
     # Генерация "количеств" некоторых главных объектов
     num_books = Book.objects.all().count()
     num_instances = BookInstance.objects.all().count()
@@ -12,29 +17,28 @@ def index(request):
     num_instances_available = (BookInstance.objects.filter(status='a').count())
     crime_books = Book.objects.filter(title__icontains='crime').count()
     genre_books = Book.objects.filter(genre__name__iexact="novel").count()
-
-    num_authors=Author.objects.count()
     # переменной context
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_visits': num_visits,
         'crime_books': crime_books,
         'genre_books': genre_books,
     }
     return render(request, 'index.html', context=context)
 
-class BookListView(generic.ListView):
+class BookListView(LoginRequiredMixin, generic.ListView):
         model = Book
         paginate_by = 4
 
-class BookDetailView(generic.DetailView):
+class BookDetailView(LoginRequiredMixin, generic.DetailView):
     model = Book
 
-class AuthorListView(generic.ListView):
+class AuthorListView(LoginRequiredMixin, generic.ListView):
         model = Author
         paginate_by = 5
 
-class AuthorDetailView(generic.DetailView):
+class AuthorDetailView(LoginRequiredMixin, generic.DetailView):
         model = Author
--- locallibrary/locallibrary/settings.py
+++ locallibrary/locallibrary/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'locallibrary.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +124,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #Initial commit
 #Initial commit
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
