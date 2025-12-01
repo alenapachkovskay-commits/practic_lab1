3842ead9933d7c746853365a59d1c9229214ea68
Admin <alenapachkovskay@gmail.com>
2025-12-01 23:29:05 +0700

    Initial commit

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
 
 urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
 ]
 
