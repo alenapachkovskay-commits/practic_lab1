7562dc62c60890fe2021b46a5aefa6408a365828
Admin <alenapachkovskay@gmail.com>
2025-12-01 19:42:01 +0700

    Initial commit

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -8,7 +8,7 @@ import datetime
 from django.contrib.auth.decorators import login_required, permission_required
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
-from catalog.forms import RenewBookForm
+from catalog.forms import RenewBookModelForm
 
 def index(request):
     """
@@ -88,7 +88,7 @@ def renew_book_librarian(request, pk):
     if request.method == 'POST':
 
         # Create a form instance and populate it with data from the request (binding):
-        form = RenewBookForm(request.POST)
+        form = RenewBookModelForm(request.POST)
 
         # Check if the form is valid:
         if form.is_valid():
@@ -102,7 +102,7 @@ def renew_book_librarian(request, pk):
     # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={'renewal_date': proposed_renewal_date})
+        form = RenewBookModelForm(initial={'renewal_date': proposed_renewal_date})
 
     context = {
         'form': form,
